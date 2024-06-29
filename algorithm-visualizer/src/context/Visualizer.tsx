c98e88d7a0d28ed8e700a91c9eb911f9af9a9c0d
Exploser <exploserbusiness@gmail.com>
2024-06-29 17:34:30 -0400

    Add general animation functions

--- algorithm-visualizer/src/context/Visualizer.tsx
+++ algorithm-visualizer/src/context/Visualizer.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { SortingAlgorithmType } from "@/lib/types";
+import { AnimationArrayType, SortingAlgorithmType } from "@/lib/types";
 import { MAX_ANIMATION_SPEED, generateRandomNumberFromInterval } from "@/lib/utils";
 import { useContext, useEffect } from "react";
 import { createContext, useState } from "react";
@@ -16,7 +16,7 @@ interface SortingAlgorithmContextType {
     isAnimationComplete: boolean;
     setIsAnimationComplete: React.Dispatch<React.SetStateAction<boolean>>;
     resetArrayAndAnimation: () => void;
-    runAnimation: () => void;
+    runAnimation: (animations: AnimationArrayType) => void;
     requireReset: boolean;
 }
 
@@ -58,7 +58,74 @@ export const SortingAlgorithmProvider = ({ children }: { children: React.ReactNo
         setIsSorting(false);
      }
 
-    const runAnimation = () => { }
+     const runAnimation = (animations: AnimationArrayType) => {
+        setIsSorting(true);
+    
+        const inverseSpeed = (1 / animationSpeed) * 200;
+        const arrLines = document.getElementsByClassName(
+          "array-line"
+        ) as HTMLCollectionOf<HTMLElement>;
+
+        const updateClassList = (
+          indexes: number[],
+          addClassName: string,
+          removeClassName: string
+        ) => {
+          indexes.forEach((index) => {
+            arrLines[index].classList.add(addClassName);
+            arrLines[index].classList.remove(removeClassName);
+          });
+        };
+
+        const updateHeightValue = (
+          lineIndex: number,
+          newHeight: number | undefined
+        ) => {
+          arrLines[lineIndex].style.height = `${newHeight}px`;
+        };
+
+        animations.forEach((animation, index) => {
+          setTimeout(() => {
+            const [lineIndexes, isSwap] = animation;
+            if (!isSwap) {
+              updateClassList(
+                lineIndexes,
+                "change-line-color",
+                "default-line-color"
+              );
+              setTimeout(
+                () =>
+                  updateClassList(
+                    lineIndexes,
+                    "default-line-color",
+                    "change-line-color"
+                  ),
+                inverseSpeed
+              );
+            } else {
+              const [lineIndex, newHeight] = lineIndexes;
+              updateHeightValue(lineIndex, newHeight);
+            }
+          }, index * inverseSpeed);
+        });
+
+        const finalTimeout = animations.length * inverseSpeed;
+        setTimeout(() => {
+          Array.from(arrLines).forEach((line) => {
+            line.classList.add("pulse-animation", "change-line-color");
+            line.classList.remove("default-line-color");
+          });
+
+          setTimeout(() => {
+            Array.from(arrLines).forEach((line) => {
+              line.classList.remove("pulse-animation", "change-line-color");
+              line.classList.add("default-line-color");
+            });
+            setIsSorting(false);
+            setIsAnimationComplete(true);
+          }, 1000);
+        }, finalTimeout);
+      };
 
     const value = {
         arrayToSort,
@@ -75,8 +142,6 @@ export const SortingAlgorithmProvider = ({ children }: { children: React.ReactNo
         runAnimation,
         requireReset,
     }
-
-
 
     return (
         <SortingAlgorithmContext.Provider value={value}>
